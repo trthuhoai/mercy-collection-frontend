17612d856e29dda6a26bd7564c46d8774dd83cba
trthuhoai <tr.thuhoai@gmail.com>
2022-11-05 14:47:00 +0700

    layout

---
+++ src/layouts/mainLayout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from 'components/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from 'components/Footer';
+
+const MainLayout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header></Header>
+      <main className="flex-1">
+        <Outlet></Outlet>
+      </main>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <div className="text-primary">day la home</div>;
+};
+
+export default Home;
